2841a14c3b6a545d3aad8b4f69380f8a02a23627
Michael Lehmann <mikelehmann00@gmail.com>
2024-09-07 12:48:34 -0500

    Add function to play whole game

--- rps.js
+++ rps.js
@@ -27,54 +27,74 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    let winner = "";
-
-    if (humanChoice === computerChoice)
-        winner = "tie";
-
-    else {
-        if (humanChoice === "rock") {
-            if (computerChoice === "paper") {
-                winner = "computer";
-            }
-            else {
-                winner = "human";
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                winner = "human";
-            }
-            else {
-                winner = "computer";
-            }
-                
-        } else {
-            if (computerChoice === "rock") {
-                winner = "computer";
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        let winner = "";
+    
+        if (humanChoice === computerChoice)
+            winner = "tie";
+    
+        else {
+            if (humanChoice === "rock") {
+                if (computerChoice === "paper") {
+                    winner = "computer";
+                }
+                else {
+                    winner = "human";
+                }
+            } else if (humanChoice === "paper") {
+                if (computerChoice === "rock") {
+                    winner = "human";
+                }
+                else {
+                    winner = "computer";
+                }
+                    
             } else {
-                winner = "human";
+                if (computerChoice === "rock") {
+                    winner = "computer";
+                } else {
+                    winner = "human";
+                }
             }
+        }
+    
+        switch (winner) {
+            case "human":
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+                break;
+            case "computer":
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+                break;
+            case "tie":
+                console.log(`Tie. You both picked ${computerChoice}`);
+                break;
+            default:
+                console.log("Error.");
+                break;
         }
     }
 
-    switch (winner) {
-        case "human":
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore++;
-            break;
-        case "computer":
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-            computerScore++;
-            break;
-        case "tie":
-            console.log(`Tie. You both picked ${computerChoice}`);
-            break;
-        default:
-            console.log("Error.");
-            break;
+    const numRounds = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < numRounds; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`Final Scores:`);
+    console.log(`You: ${humanScore}`);
+    console.log(`Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else if (computerScore > humanScore) {
+        console.log("You lose.");
+    } else {
+        console.log("Tie!");
     }
 }
+
+playGame();
