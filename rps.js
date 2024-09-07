8e45964f5a3d308cb14b8c476048d4f2691eb382
Michael Lehmann <mikelehmann00@gmail.com>
2024-09-07 11:56:53 -0500

    Add computer choice function

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+//picks rock, paper, or scissors for the computer player
+function getComputerChoice() {
+    let randVal = Math.random();
+    if (randVal < 0.333) {
+        return "rock";
+    } else if (randVal < 0.667) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
