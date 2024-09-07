a3356492ac40c16249fae6150cdb995318712128
Michael Lehmann <mikelehmann00@gmail.com>
2024-09-07 12:07:12 -0500

    Add player choice function

--- rps.js
+++ rps.js
@@ -9,3 +9,21 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let goAgain = true;
+    let choice = "";
+
+    //loop runs until we get valid input
+    while (goAgain) {
+        choice = prompt("Choose rock, paper, or scissors:");
+
+        if (choice && (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors")) {
+            goAgain = false;
+            return choice.toLowerCase();
+        } else {
+            alert("Please choose rock, paper, or scissors. Make sure your spelling is correct.");
+        }
+    }
+}
+
